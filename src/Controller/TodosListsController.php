5cd3c886481ce92311eb9992b3e101ed28eccef2
ThomasBrossier <98547958+ThomasBrossier@users.noreply.github.com>
2022-06-10 13:27:08 +0200

    Add createList Base Template & create function to send in database

--- src/Controller/TodosListsController.php
+++ src/Controller/TodosListsController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Todoslists;
+use App\Form\TodosListsType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TodosListsController extends AbstractController
 {
-    #[Route('/todos/lists', name: 'app_todos_lists')]
+    #[Route('/', name: 'app_todos_lists')]
     public function index(): Response
     {
         return $this->render('todos_lists/index.html.twig', [
             'controller_name' => 'TodosListsController',
+        ]);
+    }
+    #[Route('/create-list', name: 'app_todos_lists')]
+    public function create(Request $request,EntityManagerInterface $em): Response
+    {
+        $todosList = new Todoslists;
+        $form = $this->createForm(TodosListsType::class,$todosList);
+        $form->handleRequest($request);
+
+        if($form-> isSubmitted() && $form-> isValid()){
+           $em->persist($todosList);
+           $em->flush();
+        }
+        return $this->render('todos_lists/create.html.twig', [
+            'controller_name' => 'createListController',
+            "form"=> $form->createView()
         ]);
     }
 }
--- src/Form/TodosListsType.php
+++ src/Form/TodosListsType.php
@@ -3,7 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Todoslists;
-use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
